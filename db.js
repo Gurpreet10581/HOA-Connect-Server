e1bd8fcdb9c956e4694c67d3b4990314f87c8f45
Gurpreet Singh <er.gurpreet.2017@gmail.com>
2020-09-30 14:56:54 -0400

    intial commit server

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
+require('dotenv').config()
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.NAME, 'postgres', process.env.PASS, {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, { 
     dialect: 'postgres'
 });
 
